542db9ccb801c00cc9926b83ded152405687bb54
Ujstor <astipan@gmail.com>
2024-02-26 18:05:22 +0100

    logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ujstor/snippetbox/internal/server"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// f, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
+	// if err != nil {
+	// log.Fatal(err)
+	// }
+	// defer f.Close()
+	// infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+	
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -18,10 +33,16 @@ func main() {
 	mux.HandleFunc("/snippet/view", server.SnippetView)
 	mux.HandleFunc("/snippet/create", server.SnippetCreate)
 
-	log.Printf("Starting server on port 8088")
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
 
-	err := http.ListenAndServe(":8088", mux)
+	infoLog.Printf("Starting server on port %s", *addr)
+
+	err := srv.ListenAndServe() 
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		errorLog.Fatal(err)
 	}
 }
