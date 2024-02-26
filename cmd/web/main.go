1f926372b891f82c3d5807766947e6fcd77f702c
Ujstor <astipan@gmail.com>
2024-02-26 23:13:14 +0100

    dependency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,32 +16,30 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// f, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
-	// if err != nil {
-	// log.Fatal(err)
-	// }
-	// defer f.Close()
-	// infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
-	
+	app := &server.Application{
+		ErrorLog: errorLog,
+		InfoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", server.Home)
-	mux.HandleFunc("/snippet/view", server.SnippetView)
-	mux.HandleFunc("/snippet/create", server.SnippetCreate)
+	mux.HandleFunc("/", app.Home)
+	mux.HandleFunc("/snippet/view", app.SnippetView)
+	mux.HandleFunc("/snippet/create", app.SnippetCreate)
 
 	srv := &http.Server{
-		Addr: *addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler:  mux,
 	}
 
 	infoLog.Printf("Starting server on port %s", *addr)
 
-	err := srv.ListenAndServe() 
+	err := srv.ListenAndServe()
 	if err != nil {
 		errorLog.Fatal(err)
 	}
--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -8,10 +8,15 @@ import (
 	"strconv"
 )
 
+type Application struct {
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+}
+
 // Handler
-func Home(w http.ResponseWriter, r *http.Request) {
+func(app *Application) Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -23,34 +28,31 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
+		app.serverError(w, err)
 	}
 }
 
-func SnippetView(w http.ResponseWriter, r *http.Request) {
+func(app *Application) SnippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
 	fmt.Fprintf(w, "Specific snippet with id: %d", id)
 }
 
-func SnippetCreate(w http.ResponseWriter, r *http.Request) {
+func(app *Application) SnippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create Snippet"))
